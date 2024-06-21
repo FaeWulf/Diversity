9a8250735346dd872822550eae0eb9e29efcfe54
Faewulf <ngolamaz3@gmail.com>
2024-06-21 07:56:39 +0700

    Bundle place block feature

---
+++ src/main/java/faewulf/diversity/event/changeBunndleMode.java
@@ -0,0 +1,50 @@
+package faewulf.diversity.event;
+
+import faewulf.diversity.inter.ICustomBundleItem;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.minecraft.item.BundleItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+
+public class changeBunndleMode {
+    static public void run() {
+
+        AttackBlockCallback.EVENT.register(((player, world, hand, pos, direction) -> {
+            if (world.isClient)
+                return ActionResult.PASS;
+
+            if (hand == Hand.MAIN_HAND) {
+                ItemStack stack = player.getMainHandStack();
+                if (stack != null && !stack.isEmpty()) {
+                    if (stack.getItem() instanceof BundleItem bundleItem && player.isSneaking()) {
+
+                        int mode = ((ICustomBundleItem) bundleItem).getMode(stack);
+
+                        mode++;
+                        if (mode > 2)
+                            mode = 0;
+
+                        ((ICustomBundleItem) bundleItem).setMode(stack, mode);
+
+                        String modeText = switch (mode) {
+                            case 1 -> "Place first slot";
+                            case 2 -> "Place random slot";
+                            default -> "Normal";
+                        };
+
+                        player.sendMessage(Text.literal("Change mode to: " + modeText), true);
+                        world.playSound(null, pos, SoundEvents.ITEM_BUNDLE_INSERT, SoundCategory.PLAYERS, 0.5f, 1.5f);
+
+                        return ActionResult.CONSUME;
+                    }
+                }
+            }
+
+            return ActionResult.PASS;
+        }));
+    }
+}
