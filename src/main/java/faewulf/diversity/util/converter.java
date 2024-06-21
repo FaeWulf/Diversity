79c845363510b24ef3149a5029f66ad99e30a82b
Faewulf <ngolamaz3@gmail.com>
2024-06-21 07:57:07 +0700

    Added method to get custom enchant registry

--- src/main/java/faewulf/diversity/util/converter.java
+++ src/main/java/faewulf/diversity/util/converter.java
@@ -5,11 +5,22 @@ import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 
 public class converter {
     public static RegistryEntry<Enchantment> getEnchant(World world, RegistryKey<Enchantment> enchant) {
-        RegistryEntryLookup<Enchantment> registryEntryLookup = world.getRegistryManager().createRegistryLookup().getOrThrow(RegistryKeys.ENCHANTMENT);
+        RegistryEntryLookup<Enchantment> registryEntryLookup = world.getRegistryManager()
+                .createRegistryLookup()
+                .getOrThrow(RegistryKeys.ENCHANTMENT);
+
         return registryEntryLookup.getOrThrow(enchant);
+    }
+
+    public static RegistryEntry<Enchantment> getEnchant(World world, String namespace, String path) {
+        return world.getRegistryManager().get(RegistryKeys.ENCHANTMENT)
+                .getEntry(Identifier.of(namespace, path))
+                .orElseThrow(() -> new IllegalArgumentException("Enchantment with id " + path + " not found"));
+
     }
 }
