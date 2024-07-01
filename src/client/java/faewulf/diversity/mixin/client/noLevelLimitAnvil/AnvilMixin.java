5277757d074e80bf356afd9309feb724f8857501
Faewulf <ngolamaz3@gmail.com>
2024-07-01 10:43:36 +0700

    Fixed #4. Todo: change all redirect into WrapOperation

--- src/client/java/faewulf/diversity/mixin/client/noLevelLimitAnvil/AnvilMixin.java
+++ src/client/java/faewulf/diversity/mixin/client/noLevelLimitAnvil/AnvilMixin.java
@@ -1,17 +1,18 @@
 package faewulf.diversity.mixin.client.noLevelLimitAnvil;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import faewulf.diversity.util.ModConfigs;
 import net.minecraft.client.gui.screen.ingame.AnvilScreen;
 import net.minecraft.entity.player.PlayerAbilities;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = AnvilScreen.class)
 public abstract class AnvilMixin {
-    @Redirect(method = "drawForeground", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerAbilities;creativeMode:Z", opcode = Opcodes.GETFIELD))
-    private boolean drawForeGroundRedirect(PlayerAbilities playerAbilities) {
-        return playerAbilities.creativeMode || ModConfigs.no_level_limit_anvil;
+    @WrapOperation(method = "drawForeground", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerAbilities;creativeMode:Z", opcode = Opcodes.GETFIELD))
+    private boolean drawForeGroundWrap(PlayerAbilities instance, Operation<Boolean> original) {
+        return instance.creativeMode || ModConfigs.no_level_limit_anvil;
     }
 }
