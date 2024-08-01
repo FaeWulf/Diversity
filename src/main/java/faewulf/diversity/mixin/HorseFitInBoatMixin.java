3f103238b3045bfe36dc69c197591f1bdf4971f0
faewulf <ngolamaz3@gmail.com>
2024-08-01 21:57:06 +0700

    Added camel to the list

--- src/main/java/faewulf/diversity/mixin/HorseFitInBoatMixin.java
+++ src/main/java/faewulf/diversity/mixin/HorseFitInBoatMixin.java
@@ -2,6 +2,7 @@ package faewulf.diversity.mixin;
 
 import faewulf.diversity.util.ModConfigs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.CamelEntity;
 import net.minecraft.entity.passive.DonkeyEntity;
 import net.minecraft.entity.passive.HorseEntity;
 import net.minecraft.entity.passive.MuleEntity;
@@ -14,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(BoatEntity.class)
 public class HorseFitInBoatMixin {
 
-    //just modify return value of isSmallerThanBoat to true for horse/donkey only
+    //just modify return value of isSmallerThanBoat to true for horse/donkey and camel only
     @Inject(method = "isSmallerThanBoat", at = @At("RETURN"), cancellable = true)
     private void isSmallerThanBoatInject(Entity entity, CallbackInfoReturnable<Boolean> cir) {
 
@@ -22,7 +23,7 @@ public class HorseFitInBoatMixin {
         if (!ModConfigs.horse_can_seat_on_boat)
             return;
 
-        if (entity instanceof HorseEntity || entity instanceof DonkeyEntity || entity instanceof MuleEntity) {
+        if (entity instanceof HorseEntity || entity instanceof DonkeyEntity || entity instanceof MuleEntity || entity instanceof CamelEntity) {
             cir.setReturnValue(true);
             cir.cancel();
         }
