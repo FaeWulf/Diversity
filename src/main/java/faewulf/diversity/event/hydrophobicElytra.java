6c09a13fd2207ec1d34f2a680eda143e96bb22e3
FaeWulfs <ngolamaz3@gmail.com>
2024-08-09 18:42:45 +0700

    Fixed feature issue

--- src/main/java/faewulf/diversity/event/hydrophobicElytra.java
+++ src/main/java/faewulf/diversity/event/hydrophobicElytra.java
@@ -7,18 +7,20 @@ import net.minecraft.server.network.ServerPlayerEntity;
 public class hydrophobicElytra {
     public static void run() {
 
-        if (!ModConfigs.hydrophobic_elytra)
-            return;
 
         EntityElytraEvents.CUSTOM.register(((entity, tickElytra) -> {
+            if (!ModConfigs.hydrophobic_elytra)
+                return false;
 
             if (entity instanceof ServerPlayerEntity serverPlayerEntity) {
                 if (serverPlayerEntity.isFallFlying() &&
                         (serverPlayerEntity.isInFluid() || serverPlayerEntity.isTouchingWaterOrRain())
-                )
+                ) {
                     serverPlayerEntity.stopFallFlying();
+                    return true;
+                }
             }
-            return true;
+            return false;
         }));
     }
 }
