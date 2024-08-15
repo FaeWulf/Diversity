3b399dea59d01eb468bd2945c22127bf0f682b2f
FaeWulfs <ngolamaz3@gmail.com>
2024-08-15 16:17:18 +0700

    Update lib, hopefully fixes issue #9

--- src/main/java/faewulf/diversity/Diversity.java
+++ src/main/java/faewulf/diversity/Diversity.java
@@ -6,19 +6,12 @@ import faewulf.diversity.util.ModConfigs;
 import faewulf.diversity.util.ReflectionUtils;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.tag.TagKey;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Diversity implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("diversity");
     public static final String MODID = "diversity";
-    public static ModConfigs configs;
-
-    public static final Identifier modIdentifier = Identifier.of(MODID);
 
     @Override
     public void onInitialize() {
@@ -39,8 +32,6 @@ public class Diversity implements ModInitializer {
             ReflectionUtils.invokeMethodOnClasses("faewulf.diversity.command", "register", CommandDispatcher.class, dispatcher);
         });
     }
-
-    private static final TagKey<Enchantment> NON_TREASURE_TAG = TagKey.of(RegistryKeys.ENCHANTMENT, Identifier.of("minecraft:non_treasure"));
 
     private void loadEvent() {
         LOGGER.info("Register events...");
