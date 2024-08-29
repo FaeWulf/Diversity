d33a5083834ecbad27aaa6cf5ca12d5f967c54bf
FaeWulfs <ngolamaz3@gmail.com>
2024-08-29 08:29:07 +0700

    Fix issue #12

--- common/src/main/java/xyz/faewulf/diversity/mixin/functionalNameTag/babyNameTag.java
+++ common/src/main/java/xyz/faewulf/diversity/mixin/functionalNameTag/babyNameTag.java
@@ -5,6 +5,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,11 +13,14 @@ import xyz.faewulf.diversity.util.ModConfigs;
 
 @Mixin(AgeableMob.class)
 public abstract class babyNameTag extends PathfinderMob {
+    @Shadow
+    public abstract boolean isBaby();
+
     protected babyNameTag(EntityType<? extends PathfinderMob> entityType, Level world) {
         super(entityType, world);
     }
 
-    @Inject(method = "ageUp(IZ)V", at = @At("HEAD"), cancellable = true)
+    @Inject(at = {@At("HEAD")}, method = {"ageUp*"}, cancellable = true)
     private void growUp(int age, boolean overGrow, CallbackInfo ci) {
 
         if (!ModConfigs.baby_nametag)
@@ -30,5 +34,22 @@ public abstract class babyNameTag extends PathfinderMob {
         if (name.contains("baby")) {
             ci.cancel();
         }
+    }
+
+    @Inject(at = {@At("HEAD")}, method = {"setAge"}, cancellable = true)
+    private void setAge(int i, CallbackInfo cir) {
+
+        if (!ModConfigs.baby_nametag)
+            return;
+
+        if (this.getCustomName() == null)
+            return;
+
+        String name = this.getCustomName().getString().toLowerCase();
+
+        if (name.contains("baby") && this.isBaby()) {
+            cir.cancel();
+        }
+
     }
 }
