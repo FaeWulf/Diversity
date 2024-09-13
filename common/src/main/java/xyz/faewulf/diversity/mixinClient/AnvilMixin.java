d326d595dbe70f3529acfa44afa4d9b32e453eca
FaeWulfs <ngolamaz3@gmail.com>
2024-09-13 17:52:09 +0700

    Change to other approach

--- common/src/main/java/xyz/faewulf/diversity/mixinClient/AnvilMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixinClient/AnvilMixin.java
@@ -1,17 +1,18 @@
 package xyz.faewulf.diversity.mixinClient;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.gui.screens.inventory.AnvilScreen;
-import net.minecraft.world.entity.player.Abilities;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import xyz.faewulf.diversity.util.config.ModConfigs;
 
 @Mixin(value = AnvilScreen.class)
 public abstract class AnvilMixin {
-    @WrapOperation(method = "renderLabels", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/player/Abilities;instabuild:Z", opcode = 180))
-    private boolean drawForeGroundWrap(Abilities instance, Operation<Boolean> original) {
-        return instance.instabuild || ModConfigs.no_level_limit_anvil;
+    @ModifyExpressionValue(method = "renderLabels", at = @At(value = "CONSTANT", args = "intValue=40", ordinal = 0))
+    private int modifyLevelLimit(int original) {
+        if (ModConfigs.no_level_limit_anvil)
+            return Integer.MAX_VALUE;
+        else
+            return original;
     }
 }
