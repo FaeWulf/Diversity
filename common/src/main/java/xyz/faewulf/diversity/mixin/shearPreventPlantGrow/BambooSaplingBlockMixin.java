f8110dfd14c06fd77c47b8957c4f5f8907e44562
FaeWulfs <ngolamaz3@gmail.com>
2024-09-27 08:33:23 +0700

    + Add: Bamboo sapling to the mixin.

---
+++ common/src/main/java/xyz/faewulf/diversity/mixin/shearPreventPlantGrow/BambooSaplingBlockMixin.java
@@ -0,0 +1,42 @@
+package xyz.faewulf.diversity.mixin.shearPreventPlantGrow;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.BambooSaplingBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.faewulf.diversity.feature.entity.PseudoBlockEntity.PseudoBlockEntities;
+import xyz.faewulf.diversity.feature.entity.PseudoBlockEntity.PseudoBlockEntity;
+import xyz.faewulf.diversity.util.config.ModConfigs;
+import xyz.faewulf.diversity.util.pseudoBlockEntityUtil;
+
+import java.util.Objects;
+
+@Mixin(BambooSaplingBlock.class)
+public abstract class BambooSaplingBlockMixin extends Block {
+
+    public BambooSaplingBlockMixin(Properties p_49795_) {
+        super(p_49795_);
+    }
+
+    @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
+    private void tickInject(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom, CallbackInfo ci) {
+
+        if (!ModConfigs.shear_prevent_growing)
+            return;
+
+        PseudoBlockEntity pseudoBlockEntity = pseudoBlockEntityUtil.getBlockEntity(pLevel, pPos);
+
+        if (pseudoBlockEntity == null || pseudoBlockEntity.diversity_Multiloader$getEntityType() == null)
+            return;
+
+        if (Objects.equals(pseudoBlockEntity.diversity_Multiloader$getEntityType(), PseudoBlockEntities.STOP_GROW.getDiversity_type())) {
+            ci.cancel();
+        }
+    }
+}
