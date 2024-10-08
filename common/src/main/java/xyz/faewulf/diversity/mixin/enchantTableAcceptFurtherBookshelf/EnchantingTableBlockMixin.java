c1e7c8fc9a01589e068ebb10bd035f5ce9e75f1d
FaeWulfs <ngolamaz3@gmail.com>
2024-10-08 08:20:37 +0700

    ~ Change: Toggleable feature via mixin plugin.

--- common/src/main/java/xyz/faewulf/diversity/mixin/enchantTableAcceptFurtherBookshelf/EnchantingTableBlockMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixin/enchantTableAcceptFurtherBookshelf/EnchantingTableBlockMixin.java
@@ -5,14 +5,17 @@ import net.minecraft.world.level.block.EnchantmentTableBlock;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import xyz.faewulf.diversity.util.config.ModConfigs;
+import xyz.faewulf.diversity.util.mixinPlugin.ConditionalMixin;
 
 import java.util.List;
 
+@ConditionalMixin(configClass = ModConfigs.class, fieldName = "bigger_radius_bookshelf_for_enchantingTable")
 @Mixin(EnchantmentTableBlock.class)
 public class EnchantingTableBlockMixin {
 
     @Shadow
-    public static @Final List<BlockPos> BOOKSHELF_OFFSETS = BlockPos.betweenClosedStream(-3, -1, -3, 3, 1, 3)
+    public static final @Final List<BlockPos> BOOKSHELF_OFFSETS = BlockPos.betweenClosedStream(-3, -1, -3, 3, 1, 3)
             .filter(pos -> Math.abs(pos.getX()) >= 2 || Math.abs(pos.getZ()) >= 2)
             .map(BlockPos::immutable)
             .toList();
