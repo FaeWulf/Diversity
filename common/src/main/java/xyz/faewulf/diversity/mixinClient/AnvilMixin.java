203c374e486d76b9a8d6a010bf82c666504f7d50
FaeWulfs <ngolamaz3@gmail.com>
2024-10-08 08:16:41 +0700

    ~ Change: Disable feature via mixin plugins.

--- common/src/main/java/xyz/faewulf/diversity/mixinClient/AnvilMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixinClient/AnvilMixin.java
@@ -1,19 +1,18 @@
 package xyz.faewulf.diversity.mixinClient;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.gui.screens.inventory.AnvilScreen;
-import net.minecraft.world.entity.player.Abilities;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import xyz.faewulf.diversity.util.config.ModConfigs;
+import xyz.faewulf.diversity.util.mixinPlugin.ConditionalMixin;
 
+@ConditionalMixin(configClass = ModConfigs.class, fieldName = "no_level_limit_anvil")
 @Mixin(value = AnvilScreen.class)
 public abstract class AnvilMixin {
     @ModifyExpressionValue(method = "renderLabels", at = @At(value = "CONSTANT", args = "intValue=40", ordinal = 0))
     private int modifyLevelLimit(int original) {
-        if(ModConfigs.no_level_limit_anvil)
+        if (ModConfigs.no_level_limit_anvil)
             return Integer.MAX_VALUE;
         else
             return original;
