6dac446119cf71c20d9c7e7b6a9503257f89d7b0
FaeWulfs <ngolamaz3@gmail.com>
2024-10-08 08:22:47 +0700

    ~ Add: Ported shear prevents plant grow feature.

--- common/src/main/java/xyz/faewulf/diversity/event/useShearOnBlock.java
+++ common/src/main/java/xyz/faewulf/diversity/event/useShearOnBlock.java
@@ -1,24 +1,28 @@
 package xyz.faewulf.diversity.event;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Display;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.SugarCaneBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.BlockHitResult;
+import xyz.faewulf.diversity.feature.entity.pseudoBlockEntity.PseudoBlockEntities;
+import xyz.faewulf.diversity.feature.entity.pseudoBlockEntity.PseudoBlockEntity;
+import xyz.faewulf.diversity.util.compare;
 import xyz.faewulf.diversity.util.config.ModConfigs;
-
+import xyz.faewulf.diversity.util.pseudoBlockEntityUtil;
 
 public class useShearOnBlock {
     public static InteractionResult run(Level world, Player player, InteractionHand hand, BlockHitResult hitResult) {
 
 
-        if (!ModConfigs.check_villager_schedule)
+        if (!ModConfigs.shear_prevent_growing)
             return InteractionResult.PASS;
 
         if (world.isClientSide)
@@ -28,20 +32,35 @@ public class useShearOnBlock {
 
         if (item == Items.SHEARS) {
 
-            BlockPos blockPos = hitResult.getBlockPos();
-            BlockState blockState = world.getBlockState(blockPos);
+            BlockState block = world.getBlockState(hitResult.getBlockPos());
 
-            //sugar cane
-            if (blockState.getBlock() instanceof SugarCaneBlock) {
+            System.out.println("lmao");
 
-                IntegerProperty AGE = SugarCaneBlock.AGE;
-                world.setBlock(blockPos, blockState.setValue(AGE, 15), 4);
+            if (compare.isHasTag(block.getBlock(), "diversity:trimmable") || compare.isHasTag(block.getBlock(), "minecraft:saplings")) {
 
+                if (pseudoBlockEntityUtil.getBlockEntity(world, hitResult.getBlockPos()) != null)
+                    return InteractionResult.PASS;
+
+                Display display = PseudoBlockEntities.STOP_GROW.build(world);
+
+                BlockPos pos = hitResult.getBlockPos();
+                display.setPos(pos.getCenter().x, pos.getCenter().y, pos.getCenter().z);
+
+                if (display instanceof PseudoBlockEntity pseudoBlockEntity) {
+                    if (!pseudoBlockEntity.diversity_Multiloader$isBlockEntityAlreadyExist()) {
+                        world.addFreshEntity(display);
+                        world.playSound(null, pos, SoundEvents.GROWING_PLANT_CROP, SoundSource.PLAYERS, 1.0F, (1.0F + world.getRandom().nextFloat() * 0.2F) * 0.7F);
+
+                        player.getItemInHand(hand).hurtAndBreak(1, player, player1 -> player1.broadcastBreakEvent(hand));
+                    }
+                }
+
+                player.swing(hand, true);
                 return InteractionResult.CONSUME;
             }
         }
 
-
         return InteractionResult.PASS;
+
     }
 }
