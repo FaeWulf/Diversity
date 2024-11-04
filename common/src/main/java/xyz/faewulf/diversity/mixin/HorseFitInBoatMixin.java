2d2202fa5660e34728a0f22619f8908c39ec22e7
FaeWulfs <ngolamaz3@gmail.com>
2024-11-04 17:04:12 +0700

    ~ Change: Horse fit into boat ported to 1.21.3

--- common/src/main/java/xyz/faewulf/diversity/mixin/HorseFitInBoatMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixin/HorseFitInBoatMixin.java
@@ -5,21 +5,19 @@ import net.minecraft.world.entity.animal.camel.Camel;
 import net.minecraft.world.entity.animal.horse.Donkey;
 import net.minecraft.world.entity.animal.horse.Horse;
 import net.minecraft.world.entity.animal.horse.Mule;
-import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.entity.vehicle.AbstractBoat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.faewulf.diversity.util.config.ModConfigs;
 
-@Mixin(Boat.class)
+@Mixin(AbstractBoat.class)
 public class HorseFitInBoatMixin {
 
     //just modify return value of isSmallerThanBoat to true for horse/donkey and camel only
     @Inject(method = "hasEnoughSpaceFor", at = @At("RETURN"), cancellable = true)
     private void isSmallerThanBoatInject(Entity entity, CallbackInfoReturnable<Boolean> cir) {
-
-
         if (!ModConfigs.horse_can_seat_on_boat)
             return;
 
