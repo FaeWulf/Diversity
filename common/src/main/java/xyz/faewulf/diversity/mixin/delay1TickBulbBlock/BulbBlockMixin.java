3254b255c2bfcdf18e985dab55568b1389a62fd3
FaeWulfs <ngolamaz3@gmail.com>
2024-11-04 16:55:24 +0700

    ~ Change: 1 tick delay BulbBlock ported to 1.21.3

--- common/src/main/java/xyz/faewulf/diversity/mixin/delay1TickBulbBlock/BulbBlockMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixin/delay1TickBulbBlock/BulbBlockMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CopperBulbBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.redstone.Orientation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,20 +16,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.faewulf.diversity.util.config.ModConfigs;
 
 @Mixin(CopperBulbBlock.class)
-public abstract class BulbBlobkMixin extends Block {
+public abstract class BulbBlockMixin extends Block {
     @Shadow
     public abstract void checkAndFlip(BlockState state, ServerLevel world, BlockPos pos);
 
-    public BulbBlobkMixin(Properties settings) {
+    public BulbBlockMixin(Properties settings) {
         super(settings);
     }
 
     @Inject(method = "neighborChanged", at = @At("HEAD"), cancellable = true)
-    private void updatteInject(BlockState state, Level world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify, CallbackInfo ci) {
+    private void updateInject(BlockState blockState, Level level, BlockPos blockPos, Block block, Orientation orientation, boolean p_309085_, CallbackInfo ci) {
         if (!ModConfigs.copper_bulb_tick_delay)
             return;
 
-        world.scheduleTick(pos, world.getBlockState(pos).getBlock(), 1);
+        level.scheduleTick(blockPos, level.getBlockState(blockPos).getBlock(), 1);
         ci.cancel();
     }
 
