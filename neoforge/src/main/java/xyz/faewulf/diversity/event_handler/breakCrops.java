c0f639ad19f030e779db17b85c5d1c4c4a692d10
FaeWulfs <ngolamaz3@gmail.com>
2024-11-12 13:45:06 +0700

    ~ Change: Rename file.

--- neoforge/src/main/java/xyz/faewulf/diversity/event_handler/breakCrops.java
+++ neoforge/src/main/java/xyz/faewulf/diversity/event_handler/breakCrops.java
@@ -5,11 +5,12 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import xyz.faewulf.diversity.Constants;
+import xyz.faewulf.diversity.event.xpCrops;
 
 @EventBusSubscriber(modid = Constants.MOD_ID)
 public class breakCrops {
     @SubscribeEvent
     public static void onBlockBreak(BlockEvent.BreakEvent event) {
-        xyz.faewulf.diversity.event.breakCrops.run(event.getPlayer().level(), event.getPlayer(), event.getPos(), event.getState());
+        xpCrops.run(event.getPlayer().level(), event.getPlayer(), event.getPos(), event.getState());
     }
 }
