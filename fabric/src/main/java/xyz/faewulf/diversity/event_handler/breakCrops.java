5ce8e3be8bbfdbc6a40c4f9b023de9747d0f35ae
FaeWulfs <ngolamaz3@gmail.com>
2024-11-12 13:45:06 +0700

    ~ Change: Rename file.

--- fabric/src/main/java/xyz/faewulf/diversity/event_handler/breakCrops.java
+++ fabric/src/main/java/xyz/faewulf/diversity/event_handler/breakCrops.java
@@ -1,11 +1,12 @@
 package xyz.faewulf.diversity.event_handler;
 
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import xyz.faewulf.diversity.event.xpCrops;
 
 public class breakCrops {
     public static void register() {
         PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, blockEntity) -> {
-            xyz.faewulf.diversity.event.breakCrops.run(world, player, pos, state);
+            xpCrops.run(world, player, pos, state);
         });
     }
 }
--- forge/src/main/java/xyz/faewulf/diversity/event_handler/breakCrops.java
+++ forge/src/main/java/xyz/faewulf/diversity/event_handler/breakCrops.java
@@ -5,11 +5,12 @@ import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import xyz.faewulf.diversity.Constants;
+import xyz.faewulf.diversity.event.xpCrops;
 
 @Mod.EventBusSubscriber(modid = Constants.MOD_ID)
 public class breakCrops {
     @SubscribeEvent
     public static void onBlockBreak(BlockEvent.BreakEvent event) {
-        xyz.faewulf.diversity.event.breakCrops.run(event.getPlayer().level(), event.getPlayer(), event.getPos(), event.getState());
+        xpCrops.run(event.getPlayer().level(), event.getPlayer(), event.getPos(), event.getState());
     }
 }
