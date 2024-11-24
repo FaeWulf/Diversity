b12011347bdeaa8d695b3302a98e74b3a9dcc2e6
FaeWulfs <ngolamaz3@gmail.com>
2024-11-24 11:02:02 +0700

    ~ Change: tweak enchantments a little.

--- common/src/main/java/xyz/faewulf/diversity/mixin/core/recipeLoadBlocker/RecipeManagerMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixin/core/recipeLoadBlocker/RecipeManagerMixin.java
@@ -1,35 +1,33 @@
 package xyz.faewulf.diversity.mixin.core.recipeLoadBlocker;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
-import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeAccess;
 import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.world.item.crafting.RecipeMap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.faewulf.diversity.Constants;
 import xyz.faewulf.diversity.util.config.ModConfigs;
 
-import java.util.Map;
+import java.util.SortedMap;
 
 @Mixin(RecipeManager.class)
-public abstract class RecipeManagerMixin extends SimpleJsonResourceReloadListener {
-    public RecipeManagerMixin(Gson pGson, String pDirectory) {
-        super(pGson, pDirectory);
-    }
+public abstract class RecipeManagerMixin extends SimplePreparableReloadListener<RecipeMap> implements RecipeAccess {
 
-    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At("HEAD"))
-    private void disableRecipe(Map<ResourceLocation, JsonElement> pObject, ResourceManager pResourceManager, ProfilerFiller pProfiler, CallbackInfo ci) {
-        if (!ModConfigs.bundle_recipe)
-            pObject.remove(ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "bundle"));
-
+    @Inject(
+            method = "prepare(Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)Lnet/minecraft/world/item/crafting/RecipeMap;",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/resources/SimpleJsonResourceReloadListener;scanDirectory(Lnet/minecraft/server/packs/resources/ResourceManager;Ljava/lang/String;Lcom/mojang/serialization/DynamicOps;Lcom/mojang/serialization/Codec;Ljava/util/Map;)V", shift = At.Shift.AFTER))
+    private void prepareInject(ResourceManager p_379845_, ProfilerFiller p_380058_, CallbackInfoReturnable<RecipeMap> cir, @Local SortedMap<ResourceLocation, Recipe<?>> sortedmap) {
         if (!ModConfigs.sus_sand_recipe) {
-            pObject.remove(ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "suspicious_gravel"));
-            pObject.remove(ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "suspicious_sand"));
+            sortedmap.remove(ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "suspicious_gravel"));
+            sortedmap.remove(ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "suspicious_sand"));
         }
     }
 }
--- common/src/main/java/xyz/faewulf/diversity/util/config/ModConfigs.java
+++ common/src/main/java/xyz/faewulf/diversity/util/config/ModConfigs.java
@@ -164,9 +164,6 @@ public class ModConfigs {
     @Entry(category = "entity", name = "Wandering trader announcer")
     public static boolean wandering_trader_announcer = true;
 
-    @Entry(category = "recipe", name = "Bundle recipe", require_restart = true)
-    public static boolean bundle_recipe = true;
-
     @Entry(category = "recipe", name = "Suspicious sand/gravel recipe", require_restart = true)
     public static boolean sus_sand_recipe = true;
 
@@ -181,7 +178,6 @@ public class ModConfigs {
 
     @Entry(category = "command", name = "Enable permission system")
     public static boolean permission_enable = false;
-
 
     @Entry(category = "command", name = "Emote commands")
     public static boolean emote = true;
