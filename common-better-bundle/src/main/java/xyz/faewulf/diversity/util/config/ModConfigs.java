42238f43b89d08fe0ae92c6b9a3e0046d9594548
FaeWulfs <ngolamaz3@gmail.com>
2025-01-10 18:00:03 +0700

    ~ Change: ide issue.

--- common-better-bundle/src/main/java/xyz/faewulf/diversity/util/config/ModConfigs.java
+++ common-better-bundle/src/main/java/xyz/faewulf/diversity/util/config/ModConfigs.java
@@ -5,10 +5,10 @@ public class ModConfigs {
     @Entry(category = "general", name = "Bundle's Enchantment", require_restart = true)
     public static boolean bundle_enchantment = true;
 
-    @Entry(category = "item", name = "Bundle place function")
+    @Entry(category = "general", name = "Bundle place function")
     public static boolean bundle_place_mode = true;
 
-    @Entry(category = "recipe", name = "Bundle recipe", require_restart = true)
+    @Entry(category = "general", name = "Bundle recipe", require_restart = true)
     public static boolean bundle_recipe = true;
 
 }
