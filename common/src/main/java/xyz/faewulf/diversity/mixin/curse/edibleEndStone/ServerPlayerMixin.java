cc5c6ef860060877518e9b269078458842dea73e
FaeWulfs <ngolamaz3@gmail.com>
2025-01-10 17:34:54 +0700

    # Fix: missing consumable component for 1.21.3 1.21.4.

--- common/src/main/java/xyz/faewulf/diversity/mixin/curse/edibleEndStone/ServerPlayerMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixin/curse/edibleEndStone/ServerPlayerMixin.java
@@ -4,16 +4,22 @@ import com.mojang.authlib.GameProfile;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUseAnimation;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.Consumable;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.faewulf.diversity.util.config.ModConfigs;
+
+import java.util.Collections;
+import java.util.List;
 
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin extends Player {
@@ -29,8 +35,10 @@ public abstract class ServerPlayerMixin extends Player {
         if (itemStack.getItem() == Items.END_STONE) {
             if (ModConfigs.endstone_is_cheese) {
                 itemStack.set(DataComponents.FOOD, new FoodProperties.Builder().nutrition(3).saturationModifier(0.6F).alwaysEdible().build());
+                itemStack.set(DataComponents.CONSUMABLE, new Consumable(1.5f, ItemUseAnimation.EAT, SoundEvents.GENERIC_EAT, true, Collections.emptyList()));
             } else {
                 itemStack.remove(DataComponents.FOOD);
+                itemStack.remove(DataComponents.CONSUMABLE);
             }
         }
     }
