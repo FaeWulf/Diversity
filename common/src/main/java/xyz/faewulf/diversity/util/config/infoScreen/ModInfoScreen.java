907d648785f49ae0a975c37af6a2d3f0ed892a1f
FaeWulfs <ngolamaz3@gmail.com>
2025-02-20 14:07:02 +0700

    ~ Change: cancel button width.

--- common/src/main/java/xyz/faewulf/diversity/util/config/infoScreen/ModInfoScreen.java
+++ common/src/main/java/xyz/faewulf/diversity/util/config/infoScreen/ModInfoScreen.java
@@ -129,7 +129,7 @@ public class ModInfoScreen extends Screen {
                 Button.builder(
                                 Component.literal("Close"),
                                 button -> this.onClose())
-                        .width(70).build()
+                        .width(100).build()
                 , 1
         );
 
