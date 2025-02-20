48a1b54ec86623221c4b24e4cd1ba53322bbbebe
FaeWulfs <ngolamaz3@gmail.com>
2025-02-20 16:46:57 +0700

    ~ Change: rebalance bundle enchantments.

--- common/src/main/java/xyz/faewulf/diversity/util/config/ConfigLoaderFromAnnotation.java
+++ common/src/main/java/xyz/faewulf/diversity/util/config/ConfigLoaderFromAnnotation.java
@@ -1,14 +1,12 @@
 package xyz.faewulf.diversity.util.config;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ConfigLoaderFromAnnotation {
 
     private static final Map<String, Object> defaultValues = new LinkedHashMap<>();
+    private static final Map<String, List<String>> groupOfCategories = new HashMap<>();
 
     public static Map<String, Map<String, EntryInfo>> loadConfig(Class<?> configClass) {
         Map<String, Map<String, EntryInfo>> configMap = new LinkedHashMap<>();
@@ -25,6 +23,7 @@ public class ConfigLoaderFromAnnotation {
                 String name = entry.name();
                 String info = entry.info();
                 boolean require_restart = entry.require_restart();
+                String group = entry.group();
 
                 // Add the field's value to the map
                 try {
@@ -32,7 +31,7 @@ public class ConfigLoaderFromAnnotation {
 
                     // If category map doesn't exist, create it
                     configMap.computeIfAbsent(category, k -> new LinkedHashMap<>());
-                    configMap.get(category).put(name, new EntryInfo(field, field.getName(), name, info, value, require_restart));
+                    configMap.get(category).put(name, new EntryInfo(field, field.getName(), name, info, value, require_restart, group));
 
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
@@ -56,13 +55,14 @@ public class ConfigLoaderFromAnnotation {
                 String name = entry.name();
                 String info = entry.info();
                 boolean require_restart = entry.require_restart();
+                String group = entry.group();
 
                 // Add the field's value to the map
                 try {
                     Object value = field.get(null); // Access static field value
 
                     // If category map doesn't exist, create it
-                    configMap.put(name, new EntryInfo(field, field.getName(), name, info, value, require_restart));
+                    configMap.put(name, new EntryInfo(field, field.getName(), name, info, value, require_restart, group));
 
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
@@ -83,6 +83,13 @@ public class ConfigLoaderFromAnnotation {
         return defaultValues;
     }
 
+    public static List<String> getGroups(String category) {
+        if (!groupOfCategories.containsKey(category))
+            return new ArrayList<>();
+
+        return groupOfCategories.get(category);
+    }
+
     // Method to initialize and store default values from the config class
     public static void initializeDefaults(Class<?> configClass) {
         try {
@@ -91,10 +98,26 @@ public class ConfigLoaderFromAnnotation {
                     // Capture the field's current (default) value
                     Object defaultValue = field.get(null);
 
+
                     // Store the default value in the map
                     defaultValues.put(field.getName(), defaultValue);
+
+                    // Store group into a category map
+                    Entry entry = field.getAnnotation(Entry.class);
+                    String group = entry.group();
+                    String category = entry.category();
+
+                    // If category map doesn't exist, create it
+                    groupOfCategories.computeIfAbsent(category, k -> new ArrayList<>());
+
+                    if (!groupOfCategories.get(category).contains(group))
+                        groupOfCategories.get(category).add(group);
                 }
             }
+
+            groupOfCategories.forEach((s, strings) -> {
+                Collections.sort(strings);
+            });
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
@@ -107,14 +130,24 @@ public class ConfigLoaderFromAnnotation {
         public Field targetField;
         public String humanizeName;
         public Object value;
+        public String group;
 
-        public EntryInfo(Field field, String name, String humanizeName, String info, Object value, boolean require_restart) {
+        public boolean pseudoEntry = true;
+        public boolean visibleInConfig = true;
+
+        public EntryInfo(Field field, String name, String humanizeName, String info, Object value, boolean require_restart, String group) {
             this.name = name;
             this.info = info;
             this.require_restart = require_restart;
             this.targetField = field;
             this.humanizeName = humanizeName;
             this.value = value;
+            this.group = group;
+            this.pseudoEntry = false;
+        }
+
+        public EntryInfo(String name) {
+            this.name = name;
         }
     }
 }
