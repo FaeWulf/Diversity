bcccef00f6a82344c1b043c3882d71c19117928a
FaeWulfs <ngolamaz3@gmail.com>
2025-02-21 15:17:19 +0700

    + Add: Fox auto breed #24.

---
+++ common/src/main/java/xyz/faewulf/diversity/mixin/entity/foxAutoBreed/FoxMixin.java
@@ -0,0 +1,34 @@
+package xyz.faewulf.diversity.mixin.entity.foxAutoBreed;
+
+import net.minecraft.client.renderer.block.model.Variant;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.VariantHolder;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.Fox;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.faewulf.diversity.util.config.ModConfigs;
+
+@Mixin(Fox.class)
+public abstract class FoxMixin extends Animal implements VariantHolder<Variant> {
+    protected FoxMixin(EntityType<? extends Animal> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Inject(method = "aiStep", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;finishUsingItem(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;)Lnet/minecraft/world/item/ItemStack;"))
+    private void aiStepInject(CallbackInfo ci) {
+
+        if (!ModConfigs.fox_auto_breed)
+            return;
+
+        if (!this.isBaby()) {
+            if (this.getAge() <= 0)
+                this.setInLove(null);
+        } else {
+            this.ageUp(getSpeedUpSecondsWhenFeeding(-this.getAge()), true);
+        }
+    }
+}
