16084dd78a5843c5fef3416f3c37f8a36b0fbf48
FaeWulfs <ngolamaz3@gmail.com>
2025-02-25 13:11:05 +0700

    + Add: vacuum enchantments for bundles.

--- common/src/main/java/xyz/faewulf/diversity/mixin/core/registryLoadBlocker/RegistryLoaderMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixin/core/registryLoadBlocker/RegistryLoaderMixin.java
@@ -12,9 +12,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xyz.faewulf.diversity.Constants;
 import xyz.faewulf.diversity.util.config.ModConfigs;
-import xyz.faewulf.diversity.util.data.CustomEnchantment;
+import xyz.faewulf.diversity.registry.CustomEnchantment;
 
 @Mixin(value = RegistryDataLoader.class, priority = 1)
 public abstract class RegistryLoaderMixin {
