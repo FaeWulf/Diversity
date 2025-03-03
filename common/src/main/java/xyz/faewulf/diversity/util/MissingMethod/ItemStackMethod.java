8646faae050f76e42725e134921d99535a8283ba
FaeWulfs <ngolamaz3@gmail.com>
2025-03-03 18:01:23 +0700

    ~ Change: move from VanillaGradle to NeoforgeGradle.

--- common/src/main/java/xyz/faewulf/diversity/util/MissingMethod/ItemStackMethod.java
+++ common/src/main/java/xyz/faewulf/diversity/util/MissingMethod/ItemStackMethod.java
@@ -4,8 +4,7 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 public class ItemStackMethod {
 
