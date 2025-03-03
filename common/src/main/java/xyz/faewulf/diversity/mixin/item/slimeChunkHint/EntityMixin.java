4efdf199a3cc251cd9d5c99051b8ba90c4d85d64
FaeWulfs <ngolamaz3@gmail.com>
2025-03-03 15:58:06 +0700

    ~ Change: ported to 1.21.3.

--- common/src/main/java/xyz/faewulf/diversity/mixin/item/slimeChunkHint/EntityMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixin/item/slimeChunkHint/EntityMixin.java
@@ -19,8 +19,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xyz.faewulf.diversity.util.compare;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.faewulf.lib.util.Compare;
 import xyz.faewulf.diversity.util.config.ModConfigs;
 
