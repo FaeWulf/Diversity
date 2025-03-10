d9f162a87a97c533c610f83bbe0aa77121078927
FaeWulfs <ngolamaz3@gmail.com>
2025-03-10 15:39:42 +0700

    + Add: ported to 1.21.4

---
+++ fabric-better-bundle/src/main/java/xyz/faewulf/diversity_better_bundle/Diversity.java
@@ -0,0 +1,27 @@
+package xyz.faewulf.diversity_better_bundle;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import xyz.faewulf.diversity_better_bundle.event_handler.changeBundleMode;
+
+public class Diversity implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        Constants.LOG.info("Loading");
+
+        loadEvent();
+
+        CommonClass.init();
+
+        Constants.LOG.info("Init done");
+    }
+
+    private void loadEvent() {
+        Constants.LOG.info("Register events...");
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            changeBundleMode.register();
+        });
+    }
+
+}
