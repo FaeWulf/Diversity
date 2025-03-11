fb6cb6824a07fe7a777207ac5c4617f77856c75e
FaeWulfs <ngolamaz3@gmail.com>
2025-03-11 15:21:54 +0700

    ~ Change: update source for consistency.

--- neoforge-better-bundle/src/main/java/xyz/faewulf/diversity_better_bundle/Diversity.java
+++ neoforge-better-bundle/src/main/java/xyz/faewulf/diversity_better_bundle/Diversity.java
@@ -1,13 +1,7 @@
 package xyz.faewulf.diversity_better_bundle;
 
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-import xyz.faewulf.diversity_better_bundle.CommonClass;
-import xyz.faewulf.diversity_better_bundle.Constants;
-import xyz.faewulf.lib.api.v1.config.ConfigHelper;
-import xyz.faewulf.lib.util.config.infoScreen.ModInfoScreen;
 
 @Mod(Constants.MOD_ID)
 public class Diversity {
@@ -18,16 +12,6 @@ public class Diversity {
         //MidnightConfig.init(Constants.MOD_ID, ModConfigs.class);
 
         CommonClass.init();
-
-        //config
-        ModLoadingContext.get().registerExtensionPoint(
-                IConfigScreenFactory.class,
-                () -> (client, parent) -> {
-                    ModInfoScreen modInfoScreen = (ModInfoScreen) ConfigHelper.getConfigScreen(parent, Constants.MOD_ID);
-                    modInfoScreen.setUrls(null, Constants.WEBSITE, null, Constants.SOURCE_CODE);
-                    return modInfoScreen;
-                }
-        );
 
         Constants.LOG.info("Init done");
     }
