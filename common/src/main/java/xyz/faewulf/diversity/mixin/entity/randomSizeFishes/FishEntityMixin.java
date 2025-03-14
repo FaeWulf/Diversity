5bb08823486a49134727488c6bec5b7996cfb6f0
FaeWulfs <ngolamaz3@gmail.com>
2025-03-14 11:26:09 +0700

    ~ Change: data handle from bucket, used to be handle in interface, but forge doesn't allowed to do it :(.

--- common/src/main/java/xyz/faewulf/diversity/mixin/entity/randomSizeFishes/FishEntityMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixin/entity/randomSizeFishes/FishEntityMixin.java
@@ -1,5 +1,6 @@
 package xyz.faewulf.diversity.mixin.entity.randomSizeFishes;
 
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
@@ -8,6 +9,8 @@ import net.minecraft.world.entity.animal.AbstractFish;
 import net.minecraft.world.entity.animal.Bucketable;
 import net.minecraft.world.entity.animal.Salmon;
 import net.minecraft.world.entity.animal.WaterAnimal;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -40,6 +43,19 @@ public abstract class FishEntityMixin extends WaterAnimal implements Bucketable,
     private void readAdditionalSaveDataInject(CompoundTag nbt, CallbackInfo ci) {
         if (nbt.contains("diversity:Size")) {
             this.diversity_Multiloader$size = nbt.getFloat("diversity:Size");
+        }
+        diversity_Multiloader$reCalculateSize();
+    }
+
+    @Inject(method = "saveToBucketTag", at = @At("TAIL"))
+    private void saveToBucketTagInject(ItemStack stack, CallbackInfo ci) {
+        CustomData.update(DataComponents.BUCKET_ENTITY_DATA, stack, nbtCompound -> nbtCompound.putFloat("diversity:Size", diversity_Multiloader$getSize()));
+    }
+
+    @Inject(method = "loadFromBucketTag", at = @At("TAIL"))
+    private void loadFromBucketTagInject(CompoundTag tag, CallbackInfo ci) {
+        if (tag.contains("diversity:Size")) {
+            diversity_Multiloader$setSize(tag.getFloat("diversity:Size"));
         }
         diversity_Multiloader$reCalculateSize();
     }
