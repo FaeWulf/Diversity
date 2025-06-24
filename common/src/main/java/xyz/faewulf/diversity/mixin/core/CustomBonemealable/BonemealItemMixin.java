84e0e8563e2eb177220af4d860ab57b524c103e5
FaeWulfs <ngolamaz3@gmail.com>
2025-06-24 09:28:26 +0700

    # Fix: hand bonemealing not working on forge/neoforge.

--- common/src/main/java/xyz/faewulf/diversity/mixin/core/CustomBonemealable/BonemealItemMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixin/core/CustomBonemealable/BonemealItemMixin.java
@@ -3,6 +3,7 @@ package xyz.faewulf.diversity.mixin.core.CustomBonemealable;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BoneMealItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -10,6 +11,7 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,8 +23,26 @@ public abstract class BonemealItemMixin extends Item {
         super(properties);
     }
 
-    @Inject(method = "growCrop", at = @At(value = "HEAD"), cancellable = true)
+    @Inject(method = {"growCrop"}, at = @At(value = "HEAD"), cancellable = true)
     private static void growCropInject(ItemStack stack, Level level, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        BlockState blockstate = level.getBlockState(pos);
+        if (blockstate.getBlock() instanceof ICustomBonemealable iCustomBonemealable && iCustomBonemealable.Diversity$isValidBonemealTarget(level, pos, blockstate)) {
+            if (level instanceof ServerLevel) {
+                if (iCustomBonemealable.Diversity$isBonemealSuccess(level, level.random, pos, blockstate)) {
+                    iCustomBonemealable.Diversity$performBonemeal((ServerLevel) level, level.random, pos, blockstate);
+                }
+
+                stack.shrink(1);
+            }
+
+            cir.setReturnValue(true);
+            cir.cancel();
+        }
+    }
+
+    // For forge and neoforge, they replaced vanilla code with their code which... doing the same thing? What? Why?
+    @Inject(method = {"applyBonemeal"}, at = @At(value = "HEAD"), cancellable = true, require = 0)
+    private static void growCropInjectForge(ItemStack stack, Level level, BlockPos pos, Player player, CallbackInfoReturnable<Boolean> cir) {
         BlockState blockstate = level.getBlockState(pos);
         if (blockstate.getBlock() instanceof ICustomBonemealable iCustomBonemealable && iCustomBonemealable.Diversity$isValidBonemealTarget(level, pos, blockstate)) {
             if (level instanceof ServerLevel) {
