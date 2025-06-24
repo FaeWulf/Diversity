73e9e9d9fcdce906313db2032b29e6b24ed6078b
FaeWulfs <ngolamaz3@gmail.com>
2025-06-24 09:23:17 +0700

    #Fix: Anvil mixin crash on newer neoforge version.

--- common/src/main/java/xyz/faewulf/diversity/mixin/general/noLevelLimitAnvil/AnvilBlockMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixin/general/noLevelLimitAnvil/AnvilBlockMixin.java
@@ -3,6 +3,7 @@ package xyz.faewulf.diversity.mixin.general.noLevelLimitAnvil;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.inventory.AnvilMenu;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import xyz.faewulf.diversity.util.config.ModConfigs;
 import xyz.faewulf.lib.util.mixinPlugin.ConditionalMixin;
@@ -11,7 +12,7 @@ import xyz.faewulf.lib.util.mixinPlugin.ConditionalMixin;
 @Mixin(AnvilMenu.class)
 public class AnvilBlockMixin {
 
-    @ModifyExpressionValue(method = "createResult", at = @At(value = "CONSTANT", args = "intValue=40", ordinal = 2))
+    @ModifyExpressionValue(method = {"createResult", "createResultInternal"}, at = @At(value = "CONSTANT", args = "intValue=40", ordinal = 2))
     private int ModifyLevelLimit(int original) {
 
         if (!ModConfigs.no_level_limit_anvil)
