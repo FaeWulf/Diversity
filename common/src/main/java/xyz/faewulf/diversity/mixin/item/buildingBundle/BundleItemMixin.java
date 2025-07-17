c7c1b04fe823aa7827c2ea8a93aa07839c1e41b5
FaeWulfs <ngolamaz3@gmail.com>
2025-07-17 15:49:18 +0700

    # Fix: compat with Metal bundle.

--- common/src/main/java/xyz/faewulf/diversity/mixin/item/buildingBundle/BundleItemMixin.java
+++ common/src/main/java/xyz/faewulf/diversity/mixin/item/buildingBundle/BundleItemMixin.java
@@ -375,9 +375,5 @@ public abstract class BundleItemMixin extends Item implements ICustomBundleItem 
         itemStack.set(DataComponents.LORE, new ItemLore(new ArrayList<>() {{
             add(Component.literal("Mode: " + modeText).withStyle(ChatFormatting.GRAY));
         }}));
-
-
     }
-
-
 }
