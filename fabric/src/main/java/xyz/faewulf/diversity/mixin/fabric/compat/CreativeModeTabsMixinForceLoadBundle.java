dcc1cad44a3dedf88de934042fe66d6a3d485c9a
FaeWulfs <ngolamaz3@gmail.com>
2025-07-22 21:31:27 +0700

    # Fix: #6 recipe did not show properly.

---
+++ fabric/src/main/java/xyz/faewulf/diversity/mixin/fabric/compat/CreativeModeTabsMixinForceLoadBundle.java
@@ -0,0 +1,21 @@
+package xyz.faewulf.diversity.mixin.fabric.compat;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.faewulf.diversity.util.config.ModConfigs;
+
+@Pseudo
+@Mixin(CreativeModeTabs.class)
+public class CreativeModeTabsMixinForceLoadBundle {
+    @Inject(method = "method_51328", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/CreativeModeTab$ItemDisplayParameters;enabledFeatures()Lnet/minecraft/world/flag/FeatureFlagSet;"))
+    private static void injectItemBundle(CreativeModeTab.ItemDisplayParameters itemDisplayParameters, CreativeModeTab.Output output, CallbackInfo ci) {
+        if (ModConfigs.bundle_recipe)
+            output.accept(Items.BUNDLE);
+    }
+}
