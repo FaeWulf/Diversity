c17ce0000cff98ceb59e9769f092cb328f00add3
FaeWulfs <ngolamaz3@gmail.com>
2025-08-17 20:07:43 +0700

    + Add: Option to disable day counter client-side.

---
+++ common/src/main/java/xyz/faewulf/diversity/mixinClient/general/dayCounter/ClientPacketListenerMixin.java
@@ -0,0 +1,41 @@
+package xyz.faewulf.diversity.mixinClient.general.dayCounter;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientCommonPacketListenerImpl;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.client.multiplayer.CommonListenerCookie;
+import net.minecraft.network.Connection;
+import net.minecraft.network.TickablePacketListener;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundSoundPacket;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.faewulf.diversity.util.config.ModConfigs;
+
+@Mixin(ClientPacketListener.class)
+public abstract class ClientPacketListenerMixin extends ClientCommonPacketListenerImpl implements ClientGamePacketListener, TickablePacketListener {
+    protected ClientPacketListenerMixin(Minecraft minecraft, Connection connection, CommonListenerCookie commonListenerCookie) {
+        super(minecraft, connection, commonListenerCookie);
+    }
+
+    @Inject(method = "handleSoundEvent", at = @At("HEAD"), cancellable = true)
+    private void handleSoundEventCancelDayCounterSoundInject(ClientboundSoundPacket packet, CallbackInfo ci) {
+
+        if (!ModConfigs.blacklist_day_counter)
+            return;
+
+        float epsilon = Float.MIN_NORMAL;
+        // Specific packet for day counter
+        if (packet.getSource() == SoundSource.PLAYERS
+                && packet.getSound() == SoundEvents.NOTE_BLOCK_HAT
+                && Math.abs(packet.getVolume() - 0.1f) < epsilon
+                && Math.abs(packet.getPitch() - 1.4f) < epsilon
+        ) {
+            ci.cancel();
+        }
+    }
+}
