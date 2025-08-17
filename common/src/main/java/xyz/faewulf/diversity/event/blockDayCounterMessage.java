be6056a0eff4d62f6988718bcec5b6a65a01daa2
FaeWulfs <ngolamaz3@gmail.com>
2025-08-17 22:32:56 +0700

    # Fix: nice job forge.

--- common/src/main/java/xyz/faewulf/diversity/event/blockDayCounterMessage.java
+++ common/src/main/java/xyz/faewulf/diversity/event/blockDayCounterMessage.java
@@ -10,18 +10,10 @@ public class blockDayCounterMessage {
         if (!ModConfigs.blacklist_day_counter)
             return true;
 
-        System.out.println("1");
-        System.out.println(message.getStyle());
-
         if (message.getStyle().getHoverEvent() != null) {
             HoverEvent hover = message.getStyle().getHoverEvent();
-            System.out.println("2");
-            System.out.println(hover);
 
             if (hover.action() == HoverEvent.Action.SHOW_TEXT || hover instanceof HoverEvent.ShowText) {
-                System.out.println("3");
-                System.out.println(((HoverEvent.ShowText) hover).value());
-
                 // Block day counter message
                 Component component = ((HoverEvent.ShowText) hover).value();
                 if (component != null && component.getString().equals(Constants.MOD_ID + "_day-counter")) {
--- forge/src/main/java/xyz/faewulf/diversity/event_handler/blockDayCounterMessage.java
+++ forge/src/main/java/xyz/faewulf/diversity/event_handler/blockDayCounterMessage.java
@@ -1,6 +1,6 @@
 package xyz.faewulf.diversity.event_handler;
 
-import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.client.event.SystemMessageReceivedEvent;
 import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import xyz.faewulf.diversity.Constants;
@@ -8,10 +8,7 @@ import xyz.faewulf.diversity.Constants;
 @Mod.EventBusSubscriber(modid = Constants.MOD_ID)
 public class blockDayCounterMessage {
     @SubscribeEvent
-    public static boolean onChatReceived(ClientChatReceivedEvent event) {
-
-        System.out.println("lmaooooo");
-
+    public static boolean onChatReceived(SystemMessageReceivedEvent event) {
         if (!xyz.faewulf.diversity.event.blockDayCounterMessage.run(event.getMessage())) {
             return true;
         }
