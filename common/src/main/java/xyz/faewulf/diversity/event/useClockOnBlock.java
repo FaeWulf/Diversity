f16dc64fe1df153a2d8a84b8d1cf14ba46c855a7
Faewulf <ngolamaz3@gmail.com>
2026-08-20 16:10:02 +0700

    #Fix: wrong function use.

--- common/src/main/java/xyz/faewulf/diversity/event/useClockOnBlock.java
+++ common/src/main/java/xyz/faewulf/diversity/event/useClockOnBlock.java
@@ -1,24 +1,19 @@
 package xyz.faewulf.diversity.event;
 
 import net.minecraft.ChatFormatting;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.Display;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
-import xyz.faewulf.diversity.feature.entity.pseudoBlockEntity.PseudoBlockEntities;
-import xyz.faewulf.diversity.feature.entity.pseudoBlockEntity.PseudoBlockEntity;
 import xyz.faewulf.diversity.util.config.ModConfigs;
 import xyz.faewulf.lib.util.Converter;
 
@@ -47,19 +42,6 @@ public class useClockOnBlock {
                 for (BlockState blockState : poiType.matchingStates()) {
                     workStations.add(blockState.getBlock());
                 }
-            }
-
-            if (block.getBlock() == Blocks.SUGAR_CANE) {
-                Display display = PseudoBlockEntities.STOP_GROW.build(world);
-
-                BlockPos pos = hitResult.getBlockPos();
-                display.setPos(pos.getCenter().x, pos.getCenter().y, pos.getCenter().z);
-
-                if (display instanceof PseudoBlockEntity pseudoBlockEntity) {
-                    if (!pseudoBlockEntity.diversity_Multiloader$isBlockEntityAlreadyExist())
-                        world.addFreshEntity(display);
-                }
-
             }
 
             if (workStations.contains(block.getBlock())) {
